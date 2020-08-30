c9a5d2b1c6fb10dca822b3484dd6d3cbf6f06b76
Richard McGirr <rmcgirr83@gmail.com>
2020-08-30 05:19:25 -0500

    Remove spaces

--- event/listener.php
+++ event/listener.php
@@ -89,10 +89,7 @@ class listener implements EventSubscriberInterface
 		if ($event['ext_name'] == 'rmcgirr83/topicicononindex' && $event['action'] == 'details')
 		{
 			$this->language->add_lang('common', $event['ext_name']);
-			$this->template->assign_vars([
-				'L_BUY_ME_A_BEER_EXPLAIN' => $this->language->lang('BUY ME A BEER_EXPLAIN', '<a href="' . $this->language->lang('BUY_ME_A_BEER_URL') . '" target="_blank" rel=”noreferrer noopener”>', '</a>'),
-				'S_BUY_ME_A_BEER_TIOI' => true,
-			]);
+			$this->template->assign_var('S_BUY_ME_A_BEER_TIOI', true);
 		}
 	}
 
